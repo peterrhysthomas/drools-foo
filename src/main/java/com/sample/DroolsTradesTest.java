a1d3632193f95703b446c7e87b7722b776ba00e4
Peter Thomas <peterrhysthomas@yahoo.co.uk>
2012-11-27 14:15:34 +0000

    Added a query based solution will tidy up the rest of the code later on

--- src/main/java/com/sample/DroolsTradesTest.java
+++ src/main/java/com/sample/DroolsTradesTest.java
@@ -18,6 +18,8 @@ import org.junit.Test;
 
 public class DroolsTradesTest {
 
+	private Trade trade1, trade2, trade3, trade4, trade5, trade6;
+
 	@Test
     public void blah(){
     	KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
@@ -29,12 +31,12 @@ public class DroolsTradesTest {
     	kbase.addKnowledgePackages( kbuilder.getKnowledgePackages() );
     	StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
     	
-		Trade trade1 = new Trade("REF1", "IBM", "CPY1", "GB", 100.0, 999.99);
-		Trade trade2 = new Trade("REF2", "IBM", "CPY1", "GB", 200.0, 2000.01);
-		Trade trade3 = new Trade("REF3", "IBM", "CPY1", "FRA", 200.0, 2000.01);
-		Trade trade4 = new Trade("REF4", "APL", "CPY2", "GB", 400.0, 3000.01);
-		Trade trade5 = new Trade("REF5", "IBM", "CPY2", "GB", 500.0, 4999.99);
-		Trade trade6 = new Trade("REF6", "MCS", "CPY1", "GB", 600.0, 5000.01);
+		trade1 = new Trade("REF1", "IBM", "CPY1", "GB", 100.0, 999.99);
+		trade2 = new Trade("REF2", "IBM", "CPY1", "GB", 200.0, 2000.01);
+		trade3 = new Trade("REF3", "IBM", "CPY1", "FRA", 200.0, 2000.01);
+		trade4 = new Trade("REF4", "APL", "CPY2", "GB", 400.0, 3000.01);
+		trade5 = new Trade("REF5", "IBM", "CPY2", "GB", 500.0, 4999.99);
+		trade6 = new Trade("REF6", "MCS", "CPY1", "GB", 600.0, 5000.01);
 		
 		ksession.insert(trade1);
 		ksession.insert(trade2);
@@ -80,5 +82,22 @@ public class DroolsTradesTest {
 		for (QueryResultsRow row : results2 ) {
 			assertEquals(6.0, row.get("$tradeCount"));
 		}
+	
+		QueryResults results3 = ksession.getQueryResults("trade key count");
+		assertEquals(5, results3.size());
+		
+		Map<TradeKey, Integer> statistics = new HashMap<TradeKey, Integer>();
+		for (QueryResultsRow row : results3 ) {
+			statistics.put((TradeKey) row.get("$key"), (Integer) row.get("$tradeCount"));
+		}
+
+		assertEquals(2, statistics.get(trade1.getKey()).intValue());
+		assertEquals(2, statistics.get(trade2.getKey()).intValue());
+		assertEquals(1, statistics.get(trade3.getKey()).intValue());
+		assertEquals(1, statistics.get(trade4.getKey()).intValue());
+		assertEquals(1, statistics.get(trade5.getKey()).intValue());
+		assertEquals(1, statistics.get(trade6.getKey()).intValue());
+		
 	}
+
 }
